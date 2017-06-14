25faf279fb80635cc93a8296d219ea23337473b0
Danube Phan <dphan@devonway.com>
2017-06-13 21:07:13 -0700

    added javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-// Using Google Books API, display the author's name, the published date, an image of the book, and a link to learn more about the book.
+// Using Google Books API, display the author's name, the published date, an image of the book, and a link to learn more about the book when the user inputs a book title or author's name and clicks the search button.
+
+function search() {
+	// Store the results of the user's input
+	var user_input = document.getElementById('input').value;
+	// console.log(user_input);
+
+	// Break the user's input up by word and store in an array
+	var results = [];
+	if (user_input != null) {
+		results = user_input.split(" ");
+	}
+	
+	console.log(results);
+}
