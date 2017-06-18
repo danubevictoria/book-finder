b7464d89d890594008f19fba92cb9cc7f5291a4c
Danube Phan <dphan@devonway.com>
2017-06-18 00:51:50 -0700

    update api

--- js/main.js
+++ js/main.js
@@ -5,11 +5,31 @@ function search() {
 	var user_input = document.getElementById('input').value;
 	// console.log(user_input);
 
-	// Break the user's input up by word and store in an array
-	var results = [];
-	if (user_input != null) {
-		results = user_input.split(" ");
-	}
-	
-	console.log(results);
+	document.getElementById('results').innerHTML = "";
+
+	// AJAX calls request data from web server and displays it
+	$.ajax({
+		url: "https://www.googleapis.com/books/v1/volumes?q=" + user_input,
+		dataType: "json",
+
+		success: function(data) {
+			var results = document.getElementById('results');
+			for (i = 0; i< data.items.length; i++) {
+				results.innerHTML += '<h2>' + data.items[i].volumeInfo.title + "</h2>"
+			}
+		},
+
+		type: 'GET'
+	});
 }
+
+document.getElementById('search').addEventListener('click', search, false)
+
+google.books.load();
+
+function initialize() {
+	var viewer = new google.books.DefaultViewer(document.getElementById('viewer'));
+	viewer.load('ISBN:0738531367');
+}
+
+google.books.setOnLoadCallback(initialize);
